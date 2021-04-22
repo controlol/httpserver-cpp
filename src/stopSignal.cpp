94c7decdab741226c1b885b3921a8d3f6986efdd
controlol <laluckyluc7@gmail.com>
2021-04-22 16:03:13 +0200

    compiles but nothing happens now

--- src/stopSignal.cpp
+++ src/stopSignal.cpp
@@ -1,10 +1,14 @@
 #include "../include/stopSignal.hpp"
 
+void jemoeder(){std::cout << "hihi exiting\n";}
+
+std::function<void()> StopSignal::stopServerFunc = jemoeder;
+
 void StopSignal::setStopServerFunc(std::function<void()> f) {
-  StopSignal::stopServerFunc = f;
+  stopServerFunc = f;
 }
 
 void StopSignal::receiveSignal(int s) {
   std::cout << "[INFO] Received stop signal: " << s << "\n";
-  StopSignal::stopServerFunc();
+  stopServerFunc();
 }
